2acbfa5c07e8f268537b217ae356667dbcf298b8
Your Death <vihovaneckostik@gmail.com>
2024-11-16 10:09:42 +0200

    before creating a cqrs for auth

---
+++ server-solution/src/MedicalVisits.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,57 @@
+﻿using MedicalVisits.Models.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MedicalVisits.Infrastructure.Persistence;
+
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+    
+    
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+     
+        
+        
+        
+        builder.Entity<IdentityRole>().HasData(
+            new IdentityRole
+            {
+                Id = "1",
+                Name = "Admin",
+                NormalizedName = "ADMIN",
+                ConcurrencyStamp = Guid.NewGuid().ToString()
+            },
+            new IdentityRole
+            {
+                Id = "2",
+                Name = "Doctor",
+                NormalizedName = "DOCTOR",
+                ConcurrencyStamp = Guid.NewGuid().ToString()
+            },
+            new IdentityRole
+            {
+                Id = "3",
+                Name = "Patient",
+                NormalizedName = "PATIENT",
+                ConcurrencyStamp = Guid.NewGuid().ToString()
+            },
+            new IdentityRole
+            {
+                Id = "4",
+                Name = "Nurse",
+                NormalizedName = "NURSE",
+                ConcurrencyStamp = Guid.NewGuid().ToString()
+            }
+        );
+    }
+}
