fa0f34e3430a58cbc6e08fc2bca4006a83182a89
Your Death <65296195+Kozirojka@users.noreply.github.com>
2024-12-10 18:55:31 +0200

    Added method to hub

--- server-solution/src/MedicalVisits.Infrastructure/SignalR/Hubs/ChatHub.cs
+++ server-solution/src/MedicalVisits.Infrastructure/SignalR/Hubs/ChatHub.cs
@@ -4,5 +4,14 @@ namespace MedicalVisits.Infrastructure.SignalR.Hubs;
 
 public class ChatHub : Hub
 {
-    
+    public static int TotalViews { get; set; } = 0;
+
+
+    public async Task NewWindowsLoad()
+    {
+        TotalViews++;
+
+        await Clients.All.SendAsync("updateTotalViews", TotalViews);
+        
+    }
 }
