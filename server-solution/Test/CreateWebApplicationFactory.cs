45cee1c53af2f3bc062a47d5e9ede6be6d2e76aa
Stalker <vihovaneckostik@gmil.com>
2025-01-06 20:59:05 +0200

    created containers for dbontext its like something in docker, exist old method with deleting of all dependency

--- server-solution/Test/CreateWebApplicationFactory.cs
+++ server-solution/Test/CreateWebApplicationFactory.cs
@@ -1,7 +1,12 @@
-﻿using MedicalVisits.API;
+﻿using System.Data.Common;
+using MedicalVisits.API;
+using MedicalVisits.Infrastructure.Persistence;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+using Respawn;
 using Testcontainers.PostgreSql;
 using Xunit;
 
@@ -9,7 +14,12 @@ namespace Test;
 
 public class CreateWebApplicationFactory : WebApplicationFactory<IApiMarker>, IAsyncLifetime
 {
-
+    
+    private DbConnection _dbConnection = null!;
+    private Respawner _respawner = null!;
+    
+    public HttpClient HttpClient { get; private set; } = null!;
+    
     private readonly PostgreSqlContainer _postgresSqlContainer = new PostgreSqlBuilder()
         .WithImage("postgres:latest")
         .WithDatabase("MedicalVisits")
@@ -17,8 +27,7 @@ public class CreateWebApplicationFactory : WebApplicationFactory<IApiMarker>, IA
         .WithPassword("admin")
         .Build();
     
-    public HttpClient HttpClient { get; private set; } = null!;
-
+   
 
     //контроль життя застосунку
     public async Task InitializeAsync()
@@ -36,7 +45,28 @@ public class CreateWebApplicationFactory : WebApplicationFactory<IApiMarker>, IA
     
     
     // IWebHostBuilder дозволяє переконфігурувати сервіси у моєму застосунку
+    // protected override void ConfigureWebHost(IWebHostBuilder builder)
+    // {
+    //
+    //     string _connectionString = "Host=localhost;Port=5432;Database=MedicalVisits;Username=postgres;Password=admin";
+    //     
+    //     Environment.SetEnvironmentVariable("ConnectionStrings:DefaultConnection", _postgresSqlContainer.GetConnectionString());
+    //     
+    //     
+    //     
+    //     builder.ConfigureServices(services =>
+    //     {
+    //         services .Remove(services .SingleOrDefault(service => typeof(DbContextOptions<ApplicationDbContext>) == service.ServiceType));
+    //         
+    //         services .Remove(services.SingleOrDefault(service => typeof(DbConnection) == service.ServiceType));
+    //         
+    //         
+    //         services.AddDbContext<ApplicationDbContext>((_, option) => option.UseNpgsql(_connectionString));
+    //     });
+    // }
+    
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
+        Environment.SetEnvironmentVariable("ConnectionStrings:Postgres", _postgresSqlContainer.GetConnectionString());
     }
 }
