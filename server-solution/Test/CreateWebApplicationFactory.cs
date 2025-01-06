e5c798addd09ae9193f874811a8b2be1c3f7c5ab
Stalker <vihovaneckostik@gmil.com>
2025-01-06 19:45:35 +0200

    create implementation for CreateWebApplicationFactory.cs

--- server-solution/Test/CreateWebApplicationFactory.cs
+++ server-solution/Test/CreateWebApplicationFactory.cs
@@ -1,18 +1,42 @@
 ﻿using MedicalVisits.API;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+using Testcontainers.PostgreSql;
 using Xunit;
 
 namespace Test;
 
 public class CreateWebApplicationFactory : WebApplicationFactory<IApiMarker>, IAsyncLifetime
 {
-    public Task InitializeAsync()
+
+    private readonly PostgreSqlContainer _postgresSqlContainer = new PostgreSqlBuilder()
+        .WithImage("postgres:latest")
+        .WithDatabase("MedicalVisits")
+        .WithUsername("postgres")
+        .WithPassword("admin")
+        .Build();
+    
+    public HttpClient HttpClient { get; private set; } = null!;
+
+
+    //контроль життя застосунку
+    public async Task InitializeAsync()
     {
-        throw new NotImplementedException();
+        
+        await _postgresSqlContainer.StartAsync();
+
+        HttpClient = CreateClient();
     }
 
-    public Task DisposeAsync()
+    public async Task DisposeAsync()
     {
-        throw new NotImplementedException();
+        await _postgresSqlContainer.DisposeAsync();
+    }
+    
+    
+    // IWebHostBuilder дозволяє переконфігурувати сервіси у моєму застосунку
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
     }
 }
